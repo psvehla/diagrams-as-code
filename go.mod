d157c4bba09c26ad9ea98317def5f970481ce304
psvehla <psvehla@hotmail.com>
2022-09-03 23:50:57 +1000

    Completed sample model.

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/psvehla/diagrams-as-code
+
+go 1.19
+
+require goa.design/model v1.7.9
+
+require (
+	github.com/dimfeld/httppath v0.0.0-20170720192232-ee938bf73598 // indirect
+	github.com/dimfeld/httptreemux/v5 v5.4.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/manveru/faker v0.0.0-20171103152722-9fbc68a78c4d // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/zach-klippenstein/goregen v0.0.0-20160303162051-795b5e3961ea // indirect
+	goa.design/goa/v3 v3.7.3 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
+)
--- pkg/design/model.go
+++ pkg/design/model.go
@@ -1,9 +1,32 @@
 package design
 
-import . "goa.design/model/dsl"
+import "goa.design/model/dsl"
 
-var _ = Design("Getting Started", "This is a model of my software system.", func() {
-	var System = SoftwareSystem("Software System", "My software system.", func() {
-		Tag("system")
+var _ = dsl.Design("Getting Started", "This is a model of my software system.", func() {
+	var System = dsl.SoftwareSystem("Software System", "My software system.", func() {
+		dsl.Tag("system")
+	})
+
+	dsl.Person("User", "A user of my software system.", func() {
+		dsl.Uses(System, "Uses")
+		dsl.Tag("person")
+	})
+
+	dsl.Views(func() {
+		dsl.SystemContextView(System, "SystemContext", "An example of a System Context diagram.", func() {
+			dsl.AddAll()
+			dsl.AutoLayout(dsl.RankLeftRight)
+		})
+		dsl.Styles(func() {
+			dsl.ElementStyle("system", func() {
+				dsl.Background("#1168bd")
+				dsl.Color("#FFFFFF")
+			})
+			dsl.ElementStyle("person", func() {
+				dsl.Shape(dsl.ShapePerson)
+				dsl.Background("#08427b")
+				dsl.Color("#FFFFFF")
+			})
+		})
 	})
 })
